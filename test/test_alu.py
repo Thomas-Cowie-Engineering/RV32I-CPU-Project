aac08d0f444c25f250567f83521686380fe80a13
Thomas <Tcowie1@sheffield.ac.uk>
2026-09-16 17:27:56 +0100

    ADD mode adequately tested for I feel. 1 change I need to make tho

--- test/test_alu.py
+++ test/test_alu.py
@@ -45,7 +45,7 @@ async def test_add(uut):
   uut.rs2.value = random.randint(-(2**31),-1)
   uut.alu_control.value = 0b0000
 
-  await Timer(10, units="ns")
+  await Timer(1, units="ns")
 
   rs1_value = uut.rs1.value.to_signed()
   rs2_value = uut.rs2.value.to_signed()
@@ -54,11 +54,40 @@ async def test_add(uut):
   uut._log.info(f"Setting rs2 to {uut.rs2.value.to_signed()}")
   uut._log.info(f"Setting alu_control to {uut.alu_control.value.to_signed()}")
 
+  # Note to self: Investigate binary masks that work with negative numbers. Cannot apply AND mask here.
   expected_result = (rs1_value + rs2_value) 
   assert rd_value == expected_result, f"Test failed: Expected {expected_result}, got {rd_value}"
 
   uut._log.info(f"Test passed: {rs1_value} + {rs2_value} = {rd_value}")
   
   uut._log.info("*****************************************************************")
+
+  uut._log.info("Test 3: Testing if it adds when alu_control is not set too ADD mode.")
+
+  uut.rs1.value = 5
+  uut.rs2.value = 10
+  # Randomly chooses a mode that is not ADD (0b0000) to test if the ALU still performs addition.
+  # 1,10 and not 1,9 to set the default case.
+  uut.alu_control.value = random.randint(1,10)
+
+  await Timer(1, units="ns")
+
+  rs1_value = uut.rs1.value.to_unsigned()
+  rs2_value = uut.rs2.value.to_unsigned()
+  rd_value  = uut.rd.value.to_unsigned()
+
+  uut._log.info(f"Setting rs1 to {uut.rs1.value.to_unsigned()}")
+  uut._log.info(f"Setting rs2 to {uut.rs2.value.to_unsigned()}")
+  uut._log.info(f"Setting alu_control to {uut.alu_control.value.to_unsigned()}")
+
+  incorrect_result = (rs1_value + rs2_value) 
+  assert incorrect_result != rd_value, f"Test failed: Addition should not occur here. {rs1_value} + {rs2_value} = {incorrect_result}, rd: {rd_value}"
+  uut._log.info(f"Test passed: Addition did not occur when alu_control was set to {uut.alu_control.value.to_unsigned()}.")
+  
+
+
+
+
+
 
 
